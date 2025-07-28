dd6cc56cc1f4aaf339728bd81282f83932817b6e
Kunal Kumar <kunal.lohano@gmail.com>
2025-07-28 14:01:24 +0500

    make landing page

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import LandingPage from "./screens/LandingPage"
+import Login from "./screens/Login";
+import SignUp from "./screens/SignUp";
 
 const router = createBrowserRouter([
     {
         path:"/",
         element:<LandingPage/>
+    },
+    {
+        path:"/login",
+        element:<Login/>
+    },
+    {
+        path:"/signup",
+        element:<SignUp/>
     }
 ])
 
